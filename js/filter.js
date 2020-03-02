c4e124e684602b0d9e5eb0807f3db579a3b09ae0
TroshinSerg <serge-troshin@yandex.ru>
2020-03-02 19:59:22 +0300

    Находит элементы формы фильтра

--- js/filter.js
+++ js/filter.js
@@ -3,6 +3,10 @@
 (function () {
   var PIN_NUMBERS = 5;
   var HOUSING_TYPE = document.querySelector('#housing-type');
+  var HOUSING_PRICE = document.querySelector('#housing-price');
+  var HOUSING_ROOMS = document.querySelector('#housing-rooms');
+  var HOUSING_GUESTS = document.querySelector('#housing-guests');
+  var FEATURES = document.querySelectorAll('.map__checkbox');
 
   function checkTypeFilter(item) {
     return HOUSING_TYPE.value === 'any' || HOUSING_TYPE.value === item.offer.type;
