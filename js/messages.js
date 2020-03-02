0a62f94d723966198fe059c4907d25b1c9e96fde
TroshinSerg <serge-troshin@yandex.ru>
2020-03-02 13:42:38 +0300

    Изменяет способ проверки keycode

--- js/messages.js
+++ js/messages.js
@@ -5,12 +5,6 @@
   var SUCCESS_MESSAGE_TEMPLATE = document.querySelector('#success').content;
   var MAIN = document.querySelector('main');
 
-  var KEYCODES = {
-    esc: 27,
-    enter: 13,
-    space: 32
-  };
-
   function onErrorPopupClick(evt) {
     MAIN.querySelector('.error').remove();
     evt.currentTarget.removeEventListener('click', onErrorPopupButtonClick);
@@ -31,7 +25,7 @@
   }
 
   function onPopupEscKeydown(evt) {
-    if (evt.keyCode === KEYCODES.esc) {
+    if (window.utils.isEsc(evt.keyCode)) {
       var popupClass = MAIN.querySelector('.success') ? '.success' : '.error';
       MAIN.querySelector(popupClass).remove();
       document.removeEventListener('keydown', onPopupEscKeydown);
