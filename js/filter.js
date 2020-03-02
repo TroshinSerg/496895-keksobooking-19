a8d927b815d6d03c0d7082b7c0deb7ee882f176f
TroshinSerg <serge-troshin@yandex.ru>
2020-03-02 23:54:31 +0300

    Добавляет фильтр по цене

--- js/filter.js
+++ js/filter.js
@@ -8,12 +8,31 @@
   var HOUSING_GUESTS = document.querySelector('#housing-guests');
   var FEATURES = document.querySelectorAll('.map__checkbox');
 
+  var any = 'any';
+
+  var HousingPriceValue = {
+    MIDDLE: 'middle',
+    LOW: 'low',
+    HIGH: 'high'
+  };
+
+  var Price = {
+    TEN: 10000,
+    FIFTY: 50000
+  };
+
   function checkTypeFilter(item) {
-    return HOUSING_TYPE.value === 'any' || HOUSING_TYPE.value === item.offer.type;
+    return HOUSING_TYPE.value === any || HOUSING_TYPE.value === item.offer.type;
+  }
+
+  function checkPriceFilter(item) {
+    return HOUSING_PRICE.value === any || ((HOUSING_PRICE.value === HousingPriceValue.MIDDLE) && (item.offer.price > Price.TEN && item.offer.price < Price.FIFTY)) || ((HOUSING_PRICE.value === HousingPriceValue.LOW) && (item.offer.price < Price.TEN)) || ((HOUSING_PRICE.value === HousingPriceValue.HIGH) && (item.offer.price > Price.FIFTY));
   }
 
   function filter() {
-    return window.loadedData.filter(checkTypeFilter).slice(0, PIN_NUMBERS);
+    return window.loadedData.filter(function (item) {
+      return checkTypeFilter(item) && checkPriceFilter(item);
+    }).slice(0, PIN_NUMBERS);
   }
 
   window.filter = {
